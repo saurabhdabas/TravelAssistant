f9d036fb6beb68b3125bf0f4c40143a433c4137d
Saurabh Dabas <81917629+saurabhdabas@users.noreply.github.com>
2023-02-17 19:07:41 -0700

    Change styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,14 +7,14 @@ export default function Header({brand,username,email}) {
       <div className='header__top-section'>
         <div className='header__brand-wrapper'>
           <img className='header__brand-logo' src = "../beach1.png" alt='logo'/>
-          <h3 className='header__brand-name'>{`${brand}`}</h3>
-          <h5>Let's explore the world together</h5>
+          <h2 className='header__brand-name'>{`${brand}`}</h2>
+          <h4>Let's explore the world together</h4>
         </div>
         <h3 className='header__welcome-message'>{`Welcome, ${username}`}</h3>
       </div>
       <div className='header__bottom-section'>
-          <img className='header__avatar' src="https://i.pravatar.cc/30?img=13" alt='user-avatar'/>
-          <h5 className='header__login-detail'>{`logged in as : ${email}`}</h5>
+          <img className='header__avatar' src="https://i.pravatar.cc/40?img=13" alt='user-avatar'/>
+          <h4 className='header__login-detail'>{`logged in as : ${email}`}</h4>
       </div>
     </div>
   )
